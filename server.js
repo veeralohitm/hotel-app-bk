6bc4141120e107a641eb5baad86d225da6f359fd
Veera Lohit <veeralohitm@Veeras-MacBook-Air.local>
2025-01-23 15:26:37 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+// Next initialize the application
+const app = express();
+
+// routing path
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
