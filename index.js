6452d8b1203bb4d7c610020eec3eafd05a998d98
Veera Lohit <veeralohitm@Veeras-MacBook-Air.local>
2025-03-02 19:28:16 -0500

    new index test

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// routing path
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
